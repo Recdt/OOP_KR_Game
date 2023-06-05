a46e014236c58f2c0dca844708b9e0c4cbc79af8
Meverlk <70843129+Meverlk@users.noreply.github.com>
2023-06-05 14:34:48 +0300

    grass spawn

--- RPG_KR_Game/Assets/Scripts/SpawnSystem/MapObjSpawn.cs
+++ RPG_KR_Game/Assets/Scripts/SpawnSystem/MapObjSpawn.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Collections;
+using Interfaces.MapInterfaces;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
 namespace SpawnSystem
 {
-    public class MapObjSpawn
+    public class MapObjSpawn : MonoBehaviour, RandomPosition
     {
-        
+        [SerializeField] private BoxCollider2D map;
+        [SerializeField] private int grassAmount = 10;
+        [SerializeField] private GameObject grassExample;
+
+        private void Start()
+        {
+            StartCoroutine(spawnGrass());
+        }
+
+        public Vector2 getRandomPosition()
+        {
+            var randomPosition = new Vector2(Random.Range(-map.size.x / 2, map.size.x / 2), 
+                Random.Range(-map.size.y / 2, map.size.y / 2));
+            return randomPosition;
+        }
+        private IEnumerator spawnGrass()
+        {
+            for (int i = 0; i < grassAmount; i++)
+            {
+                Instantiate(grassExample, getRandomPosition(), Quaternion.identity, transform);
+                yield return new WaitForEndOfFrame();
+            }
+        }
     }
 }
