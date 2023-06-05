78abdd19bbdfcb14f1f12c68d86d7261aec1130f
Meverlk <70843129+Meverlk@users.noreply.github.com>
2023-06-05 23:43:42 +0300

    add grass collisions

--- RPG_KR_Game/Assets/Scripts/ObjectsScripts/Grass.cs
+++ RPG_KR_Game/Assets/Scripts/ObjectsScripts/Grass.cs
@@ -1,50 +1,47 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Interfaces.MapInterfaces;
-using Unity.VisualScripting;
 using Random = UnityEngine.Random;
-using Vector3 = System.Numerics.Vector3;
 
 namespace ObjectsScripts
 {
     public class Grass : MonoBehaviour, Grow, Decrease, Die
     {
         private MapObject _objInfo;
+        private Rigidbody2D _rb;
 
         private void Start()
         {
-            grow();
+            _rb = GetComponent<Rigidbody2D>();
+            StartCoroutine(startRigitbody());
         }
 
-        Vector3 getPosition()
+        private IEnumerator startRigitbody()
         {
-            return _objInfo.getPosition();
+            _rb.WakeUp();
+            yield return new WaitForSeconds(0.1f);
+            _rb.Sleep();
         }
 
-        private void OnCollisionStay2D(Collision2D other)
-        {
-            if (LayerMask.LayerToName(other.gameObject.layer) == "Grass")
-            {
-                transform.Translate(0.2f * (gameObject.transform.position.x - other.gameObject.transform.position.x), 
-                    0.2f * (gameObject.transform.position.y - other.gameObject.transform.position.y), 0);
-            }
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
+        private void OnTriggerStay2D(Collider2D other)
         {
             if (LayerMask.LayerToName(other.gameObject.layer) == "Mooses")
             {
-                Debug.Log("Grass was eaten");
                 die();
+            }
+            else if (LayerMask.LayerToName(other.gameObject.layer) == "Grass")
+            {
+                var thisPos = gameObject.transform.position;
+                transform.Translate(0.2f * (thisPos.x - other.gameObject.transform.position.x), 
+                    0.2f * (thisPos.y - other.gameObject.transform.position.y), 0);
             }
         }
 
         public void grow()
         {
-            var randomPosition = new Vector2(Random.Range(gameObject.transform.position.x - 0.4f, gameObject.transform.position.x + 0.4f), 
-                Random.Range(gameObject.transform.position.y - 0.4f, gameObject.transform.position.y + 0.4f));
+            var thisPos = gameObject.transform.position;
+            var randomPosition = new Vector2(Random.Range(thisPos.x - 0.4f, thisPos.x + 0.4f), 
+                Random.Range(thisPos.y - 0.4f, thisPos.y + 0.4f));
             Instantiate(gameObject, randomPosition, Quaternion.identity, transform);
         }
 
