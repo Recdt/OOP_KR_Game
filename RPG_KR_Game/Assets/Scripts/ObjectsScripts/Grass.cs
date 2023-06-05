cf8219a26d6440b3f8edf035196173b639239c81
Meverlk <70843129+Meverlk@users.noreply.github.com>
2023-06-05 19:27:30 +0300

    growing grass realisation

--- RPG_KR_Game/Assets/Scripts/ObjectsScripts/Grass.cs
+++ RPG_KR_Game/Assets/Scripts/ObjectsScripts/Grass.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Interfaces.MapInterfaces;
 using Unity.VisualScripting;
+using Random = UnityEngine.Random;
 using Vector3 = System.Numerics.Vector3;
 
 namespace ObjectsScripts
@@ -12,19 +13,23 @@ namespace ObjectsScripts
     {
         private MapObject _objInfo;
 
-        void Start ()
+        private void Start()
         {
-
-        }
-
-        void Update()
-        {
-            
+            grow();
         }
 
         Vector3 getPosition()
         {
             return _objInfo.getPosition();
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            if (LayerMask.LayerToName(other.gameObject.layer) == "Grass")
+            {
+                transform.Translate(0.2f * (gameObject.transform.position.x - other.gameObject.transform.position.x), 
+                    0.2f * (gameObject.transform.position.y - other.gameObject.transform.position.y), 0);
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -38,12 +43,14 @@ namespace ObjectsScripts
 
         public void grow()
         {
-            
+            var randomPosition = new Vector2(Random.Range(gameObject.transform.position.x - 0.4f, gameObject.transform.position.x + 0.4f), 
+                Random.Range(gameObject.transform.position.y - 0.4f, gameObject.transform.position.y + 0.4f));
+            Instantiate(gameObject, randomPosition, Quaternion.identity, transform);
         }
 
         public void decrease()
         {
-            
+            die();
         }
 
         public void die()
