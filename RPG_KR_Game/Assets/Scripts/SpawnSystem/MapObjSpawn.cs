7d2d84f0bff2f3a204d26515fcc58d8006e0f43f
Meverlk <70843129+Meverlk@users.noreply.github.com>
2023-06-05 15:00:55 +0300

    working version of grass spawn

--- RPG_KR_Game/Assets/Scripts/SpawnSystem/MapObjSpawn.cs
+++ RPG_KR_Game/Assets/Scripts/SpawnSystem/MapObjSpawn.cs
@@ -3,31 +3,35 @@ using System.Collections;
 using Interfaces.MapInterfaces;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using Vector3 = System.Numerics.Vector3;
 
 namespace SpawnSystem
 {
     public class MapObjSpawn : MonoBehaviour, RandomPosition
     {
-        [SerializeField] private BoxCollider2D map;
-        [SerializeField] private int grassAmount = 10;
-        [SerializeField] private GameObject grassExample;
+        private Vector3 _map;
+        private int _grassAmount;
+        private GameObject _grassExample;
 
-        private void Start()
+        public MapObjSpawn(Vector3 map, int grassAmount, GameObject grass)
         {
+            _map = map;
+            _grassAmount = grassAmount;
+            _grassExample = grass;
             StartCoroutine(spawnGrass());
         }
 
         public Vector2 getRandomPosition()
         {
-            var randomPosition = new Vector2(Random.Range(-map.size.x / 2, map.size.x / 2), 
-                Random.Range(-map.size.y / 2, map.size.y / 2));
+            var randomPosition = new Vector2(Random.Range((-_map.X) / 2, _map.X / 2), 
+                Random.Range(-_map.Y / 2, _map.Y / 2));
             return randomPosition;
         }
         private IEnumerator spawnGrass()
         {
-            for (int i = 0; i < grassAmount; i++)
+            for (int i = 0; i < _grassAmount; i++)
             {
-                Instantiate(grassExample, getRandomPosition(), Quaternion.identity, transform);
+                Instantiate(_grassExample, getRandomPosition(), Quaternion.identity, transform);
                 yield return new WaitForEndOfFrame();
             }
         }
