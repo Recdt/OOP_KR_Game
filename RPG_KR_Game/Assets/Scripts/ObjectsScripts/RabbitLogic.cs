7c8f1030108119179e0082d58cb1a5d02b4733c0
Relk_tp <chopyk.mykola@lll.kpi.ua>
2023-06-07 00:32:32 +0300

    Update RabbitLogic

--- RPG_KR_Game/Assets/Scripts/ObjectsScripts/RabbitLogic.cs
+++ RPG_KR_Game/Assets/Scripts/ObjectsScripts/RabbitLogic.cs
@@ -2,28 +2,143 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Interfaces;
 using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class RabbitLogic : MonoBehaviour
+public class RabbitLogic : MonoBehaviour, IDying, IStarving, ITrigger,ICollision
 {
-    [SerializeField, Range(50,100)]private float hunger = 75f;
-    private float _maxHunger;
-    [SerializeField, Range(2,5)] private float speed = 2f;
-    [SerializeField, Range(10, 40)] private float nutritionalValue = 10;
-    private List<Transform> _victim;
+    #region Properties
+    
+    [SerializeField, Range(50,100)]
+    private float hunger = 750f;
+    [SerializeField, Range(3,7)] 
+    private float speed = 5f;
+    [SerializeField, Range(10, 40)] 
+    private float nutritionalValue = 10;
+    
+    private List<Transform> _grass;
+    private List<Transform> _rabbits;
     private List<Transform> _wolfs;
-    [SerializeField] private Wandering wandering;
-    // Start is called before the first frame update
-    void Start()
+    private Wandering _wandering;
+    private float _maxHunger;
+    private bool _isRunningAway;
+    
+    #endregion
+
+    #region Methouds
+    
+    public void Starving()
     {
-        
+        hunger -= 2*Time.deltaTime;
+    }
+    public void Dying()
+    {
+        if (hunger <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void OnTriggerEnter2D(Collider2D col)//дописати втечу зайця від вовка
+    {
+        if (LayerMask.LayerToName(col.gameObject.layer) == "Wolfs")
+        {
+            var target = col.GetComponent<Transform>();
+            _wolfs.Add(target);
+        }
+        else if (LayerMask.LayerToName(col.gameObject.layer) == "Grass")
+        {
+            var target = col.GetComponent<Transform>();
+            _grass.Add(target);
+        }
+        else if (LayerMask.LayerToName(col.gameObject.layer) == "Victim")
+        {
+            var target = col.GetComponent<Transform>();
+            _rabbits.Add(target);
+        }
+        IsTargetFound();
+    }
+    
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (LayerMask.LayerToName(other.gameObject.layer) == "Wolfs")
+        {//дописати параметри погоні
+            _wolfs.Remove(other.gameObject.transform);
+        }
+        else if (LayerMask.LayerToName(other.gameObject.layer) == "Victim") _rabbits.Remove(other.gameObject.transform);
+        else if(LayerMask.LayerToName(other.gameObject.layer) == "Grass") _grass.Remove(other.gameObject.transform);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (LayerMask.LayerToName(col.gameObject.layer) == "Grass")
+        {
+            _grass.Remove(col.gameObject.transform);
+            Destroy(col.gameObject);
+            hunger += nutritionalValue;
+            IsTargetFound();
+        }
+        else if (LayerMask.LayerToName(col.gameObject.layer) == "Victim" &&
+                 hunger >= 0.5f * _maxHunger)
+        {
+            //create rabbit and -hunger
+
+        }
+        else if (LayerMask.LayerToName(col.gameObject.layer) == "Water" ||
+                 LayerMask.LayerToName(col.gameObject.layer) == "Wolf")
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void LifeCycle()
+    {
+        if (_isRunningAway)
+        {
+            
+        }
+        else if (hunger <= 0.5 * _maxHunger)
+        {
+            transform.position = Vector2.MoveTowards(transform.position,
+                _grass.First().transform.position, speed * Time.deltaTime);
+        }
+        else if(hunger > 0.5 * _maxHunger)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, 
+                _rabbits.First().transform.position, speed * Time.deltaTime);
+        }
+    }
+    private void IsTargetFound()
+    {
+        if (_grass.Count() <= 0 && _wolfs.Count() <= 0) _wandering.enabled = true;
+        else _wandering.enabled = false;
+    }
+
+    private void RunAway()
     {
         
     }
+    #endregion
+    #region StartAndUpdate
+    void Start()
+    {
+        hunger = Random.Range(100, 200);
+        _maxHunger = hunger;
+        speed = Random.Range(3, 7);
+        _grass = new List<Transform>();
+        _wolfs = new List<Transform>();
+        _rabbits = new List<Transform>();
+        hunger = 20;
+        _isRunningAway = false;
+        _wandering = GetComponent<Wandering>();
+    }
+    
+    void Update()
+    {
+        Starving();
+        Dying();
+        LifeCycle();
+    }
+    #endregion
 }
