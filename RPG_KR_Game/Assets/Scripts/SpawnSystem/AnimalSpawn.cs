3efcd4e40599b4dc326ff50e3619ae11e05fba31
Meverlk <70843129+Meverlk@users.noreply.github.com>
2023-06-08 12:43:36 +0300

    changes in scene, added animal spawn

---
+++ RPG_KR_Game/Assets/Scripts/SpawnSystem/AnimalSpawn.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
+
+namespace SpawnSystem
+{
+    public class AnimalSpawn : MonoBehaviour, ISpawn
+    {
+        private readonly RandomPosGenerator _randomPosGenerator;
+
+        public AnimalSpawn()
+        {
+            _randomPosGenerator = new RandomPosGenerator();
+        }
+
+        public IEnumerator Spawn(GameObject example, Vector3 map, int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Instantiate(example, _randomPosGenerator.GetRandomPosition(map), Quaternion.identity, transform);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
