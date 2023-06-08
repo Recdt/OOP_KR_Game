34432b2aaa1879a56095db642f4579f0699d3bb6
Meverlk <70843129+Meverlk@users.noreply.github.com>
2023-06-08 14:04:24 +0300

    added grass update spawn

--- RPG_KR_Game/Assets/Scripts/SpawnSystem/MapObjSpawn.cs
+++ RPG_KR_Game/Assets/Scripts/SpawnSystem/MapObjSpawn.cs
@@ -12,9 +12,10 @@ namespace SpawnSystem
     public class MapObjSpawn : MonoBehaviour
     {
         private Vector3 _map;
-        private int _grassAmount;
-        private int _wolfAmount;
-        private int _rabbitAmount;
+        private int _grassAmount = 400;
+        private int _grassUpdateAmount = 10;
+        private int _wolfAmount = 10;
+        private int _rabbitAmount = 30;
         
         [SerializeField] private GameObject _grassExample;
         [SerializeField] private GameObject _wolfExample;
@@ -23,22 +24,32 @@ namespace SpawnSystem
         private GrassSpawn _grassSpawn;
         private AnimalSpawn _animalSpawn;
         private Rain _rainEvent;
+        
+        private float _interval = 5f;
+        private float _timer;
 
         private void Start()
         {
             _map.X = 300;
             _map.Y = 300;
-            
-            _grassAmount = 400;
-            _wolfAmount = 10;
-            _rabbitAmount = 30;
-            
+
             _grassSpawn = gameObject.AddComponent<GrassSpawn>();
             _animalSpawn = gameObject.AddComponent<AnimalSpawn>();
             
             _rainEvent = gameObject.AddComponent<Rain>();
             _grassSpawn.SetRainEvent(_rainEvent);
             SpawnAll();
+            _timer = _interval;
+        }
+        
+        private void Update()
+        {
+            _timer -= Time.deltaTime;
+            if (_timer <= 0f)
+            {
+                StartCoroutine(_grassSpawn.Spawn(_grassExample, _map, _grassUpdateAmount));
+                _timer = _interval;
+            }
         }
 
         private void SpawnAll()
@@ -46,6 +57,11 @@ namespace SpawnSystem
             StartCoroutine(_grassSpawn.Spawn(_grassExample, _map, _grassAmount));
             StartCoroutine(_animalSpawn.Spawn(_wolfExample, _map, _wolfAmount));
             StartCoroutine(_animalSpawn.Spawn(_rabbitExample, _map, _rabbitAmount));
+        }
+
+        private void SpawnSomeGrass()
+        {
+            
         }
     }
 }
