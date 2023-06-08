d42fc124634ff807e967a39d65c8b4302f04bdbe
Dmytro Ivanenko <4workstudynstuff@gmail.com>
2023-06-08 16:40:37 +0200

    Options menu complete

--- RPG_KR_Game/Assets/ReadInput.cs
+++ RPG_KR_Game/Assets/ReadInput.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using UnityEngine;
 
 public class ReadInput : MonoBehaviour
 {
-    private string input;
+    private const string filePath = "Assets/StartData/start_data.txt";
+
+    private int grassCount = 400;
+    private int wolvesCount = 200;
+    private int rabbitsCount = 300;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,22 +25,32 @@ public class ReadInput : MonoBehaviour
         
     }
 
-    public void ReadStringInput(string s)
+    public void ReadStringInputGrass(string s)
     {
-        int numOfGrass = int.Parse(s);
-        Debug.Log(input);
+        grassCount = int.Parse(s);
     }
     
     public void ReadStringInputWolves(string s)
     {
-        input = s;
-        Debug.Log(input);
+        wolvesCount = int.Parse(s);
     }
     
     public void ReadStringInputRabbits(string s)
     {
-        input = s;
-        Debug.Log(input);
+        rabbitsCount = int.Parse(s);
     }
+
+    public void WriteData()
+    {
+        File.WriteAllText(filePath, string.Empty);
+        using (StreamWriter writer = new StreamWriter(filePath, true))
+        {
+            writer.WriteLine(grassCount);
+            writer.WriteLine(wolvesCount);
+            writer.WriteLine(rabbitsCount);
+        }
+        Debug.Log("Data was written inside of file!");
+    }
+    
 }
 
